d5e3c19bc497df4b808404c65a05fbfa74bea924
raphaelmarra <gouveiamarra@gmail.com>
2026-08-09 01:44:01 -0300

    chore(sdk): refresh swift registry hash

--- packages/ravi-os-swift-sdk/Sources/RaviSDK/RaviVersion.generated.swift
+++ packages/ravi-os-swift-sdk/Sources/RaviSDK/RaviVersion.generated.swift
@@ -3,5 +3,5 @@
 // Drift is detected by `ravi sdk swift check`.
 
 public let RAVI_SDK_VERSION = "0.1.0"
-public let RAVI_REGISTRY_HASH = "sha256:0f33382de1fc771700aeb01adee85d0a3bcbc6d2efa9a31e22a5aa234da06d29"
+public let RAVI_REGISTRY_HASH = "sha256:2389d0925c0f408c05773debd2f094c19705b8f392212fbdc8b467b2151b8e79"
 public let RAVI_GIT_SHA = "4c6ba60c804a"
